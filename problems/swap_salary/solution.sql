a33aabd409cf59a3bf2f5de8f468a149da2616a9
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-05-28 07:52:06 +0000

    Sync LeetCode submission - Swap Salary (mysql)

---
+++ problems/swap_salary/solution.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+UPDATE Salary
+    SET sex=(
+        CASE
+            WHEN sex = 'm'
+            THEN 'f'
+            ELSE 'm'
+        END
+    )
+;
