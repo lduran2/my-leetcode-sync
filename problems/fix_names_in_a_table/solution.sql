98d0126035620e1dfce855c49ed685e5c9849035
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-05-30 06:34:12 +0000

    Sync LeetCode submission - Fix Names in a Table (mysql)

---
+++ problems/fix_names_in_a_table/solution.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+
+# Table: Users
+
+# Write an SQL query to fix the names so that only the first character
+# is uppercase and the rest are lowercase.
+
+# Return the result table ordered by user_id.
+
+SELECT
+        user_id,
+        CONCAT(
+            UPPER(MID(name, 1, 1)),
+            LOWER(MID(name, 2))
+        ) AS name
+    FROM Users
+    ORDER BY user_id
+;
