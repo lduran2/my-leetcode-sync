95e3add346057c9d215660c2bf672193391c9f3c
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-06-06 19:50:31 +0000

    Sync LeetCode submission - Customers Who Never Order (mysql)

--- problems/customers_who_never_order/solution.sql
+++ problems/customers_who_never_order/solution.sql
@@ -1,10 +1,29 @@
 # Write your MySQL query statement below
 
+# Table: Customers
+
+# Table: Orders
+
+# Report all customers who never order anything.
+
+# select the name of those Customers
 SELECT name AS Customers
-    FROM Customers
-        # join from customers to catch customers with no orders
-        LEFT JOIN Orders
-            ON (Customers.id = Orders.customerId)
-    # no orders when orders.id == NULL
-    WHERE (Orders.id IS NULL)
+    FROM
+    (
+        SELECT
+                Customers.id,
+                name,
+                # count the number of Orders per each of the Customers
+                #   NULL represent no orders
+                sum(Orders.id) AS numOrders
+            FROM
+                # join Orders to Customers
+                Customers
+                LEFT JOIN
+                    Orders
+                    ON Customers.id = Orders.customerId
+            GROUP BY Customers.id
+            # NULL numOrders represent no orders
+            HAVING (numOrders IS NULL)
+    ) AS CustomersWithNumOrders
 ;
