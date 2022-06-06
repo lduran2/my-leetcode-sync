3e9f5f6162cc8c04582a40b29608902707486111
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-06-06 18:50:22 +0000

    Sync LeetCode submission - Find Customer Referee (mysql)

--- problems/find_customer_referee/solution.sql
+++ problems/find_customer_referee/solution.sql
@@ -1,6 +1,13 @@
 # Write your MySQL query statement below
 
+# Table: Customer
+
+# Report the names of the customer that are not referred by the
+# customer with id = 2.
+
 SELECT name
     FROM Customer
-    WHERE (coalesce(referee_id, -1) <> 2)
+    WHERE
+        # default to allowing NULL referee_ids
+        coalesce((referee_id <> 2), TRUE)
 ;
