625d9bcbb65a34d64120ba50c581fdc48bdb120c
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-06-07 20:27:32 +0000

    Sync LeetCode submission - Swap Salary (mysql)

--- problems/swap_salary/solution.sql
+++ problems/swap_salary/solution.sql
@@ -1,11 +1,23 @@
 # Write your MySQL query statement below
 
+# Table: Salary
+
+# Swap all 'f' and 'm' values (i.e., change all 'f' values to 'm' and
+# vice versa) with a single update statement and no intermediate
+# temporary tables.
+
+# Note that you must write a single update statement, do not write any
+# select statement for this problem.
+
 UPDATE Salary
-    SET sex=(
+    SET sex =
+    (
+        # given a sex, return the opposite
+        # return NULL if illegal value
         CASE
-            WHEN sex = 'm'
-            THEN 'f'
-            ELSE 'm'
+            WHEN sex = 'm' THEN 'f'
+            WHEN sex = 'f' THEN 'm'
+            ELSE NULL
         END
     )
 ;
