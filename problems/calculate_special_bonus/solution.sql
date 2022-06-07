c892bfe7dfe0879f4159065d337c96ac84fbac38
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-06-07 18:38:41 +0000

    Sync LeetCode submission - Calculate Special Bonus (mysql)

--- problems/calculate_special_bonus/solution.sql
+++ problems/calculate_special_bonus/solution.sql
@@ -1,19 +1,28 @@
 # Write your MySQL query statement below
 
+# Table: Employees
+
+# Calculate the bonus of each employee.  The bonus of an employee is
+# 100% of their salary if the ID of the employee is an odd number and
+# the employee name does not start with the character 'M'.  The bonus
+# of an employee is 0 otherwise.
+
 SELECT
         employee_id,
-        (
-            CASE
-                # if odd employee ID
-                WHEN (employee_id & 1)
-                    # and name does not begin with 'M'
-                    AND NOT(name LIKE 'M%')
-                # give 100% bonus
-                THEN salary
-                # otherwise, bonus = 0
-                ELSE 0
-            END
-        ) AS bonus
+        # salary if passes gets_bonus test giving bonus
+        IF(gets_bonus, salary, 0) AS bonus
     FROM
-        Employees
+    (
+        # verify whether an employee gets a bonus
+        SELECT
+                employee_id,
+                salary,
+                # use the criterion (odd ID#, name does not start in M)
+                # to choose whether the employee gets a bonus
+                (
+                    ((employee_id & 1) = 1)
+                    AND (name NOT LIKE 'M%')
+                ) AS gets_bonus
+            FROM Employees
+    ) AS EmployeesWithGetBonus
 ;
